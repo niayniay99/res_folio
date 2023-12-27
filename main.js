82cc71fe6ca0fa3c9b8d72ed267ef7ce741f051d
nilay_d <niayniay99@gmail.com>
2023-12-27 23:11:18 +0530

    This work is done in last 4 days. By Nilay

---
+++ main.js
@@ -0,0 +1,11 @@
+  
+  const typed = new Typed(".text", {
+        strings:["Software Developer" , "Youtuber" , "Web Developer"],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+    }
+    );
+    
+
